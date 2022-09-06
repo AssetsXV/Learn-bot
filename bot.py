180e9596184fd94d28a961b26f8994ff24b8b959
assetsXV <alexey.nepluev@gmail.ru>
2022-09-07 00:08:14 +0300

    New Bot

---
+++ bot.py
@@ -0,0 +1,24 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+logging.basicConfig(filename="bot.log",level=logging.INFO)
+def greet_user(update,context):
+    print('Вызван /start')
+    update.message.reply_text('Привет пользователь! Ты вызвал команду /start')
+
+def talk_to_me(update,context):
+    text=update.message.text 
+    print(text)
+    update.message.reply_text(text)
+    
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True)
+    dp=mybot.dispatcher #новая переменная для упоминания dispatcher
+    dp.add_handler(CommandHandler('Start',greet_user))
+    dp.add_handler(MessageHandler(Filters.text,talk_to_me))
+    logging.info("Бот стартовал")
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__=="__main__":
+    main()
